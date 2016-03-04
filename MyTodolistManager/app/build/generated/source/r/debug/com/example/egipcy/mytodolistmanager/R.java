14e71367d0bd233f31e0fc42e943e2121698e6e7
egipcy <egipcy@gmail.com>
2016-03-04 23:19:15 +0000

    Added files via upload
    Milestone 01

--- MyTodolistManager/app/build/generated/source/r/debug/com/example/egipcy/mytodolistmanager/R.java
+++ MyTodolistManager/app/build/generated/source/r/debug/com/example/egipcy/mytodolistmanager/R.java
@@ -15,7 +15,9 @@ public final class R {
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080002;
+        public static final int btn_task=0x7f080000;
+        public static final int et_task=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -29,7 +31,9 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f060000;
         public static final int app_name=0x7f060001;
-        public static final int hello_world=0x7f060002;
+        public static final int btn_task=0x7f060002;
+        public static final int et_task=0x7f060003;
+        public static final int hello_world=0x7f060004;
     }
     public static final class style {
         /**  Customize your theme here. 

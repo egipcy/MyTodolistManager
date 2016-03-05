b85b290f636c01e2fe800ae884c933a7206d7f6a
egipcy <egipcy@gmail.com>
2016-03-05 10:39:29 +0000

    Added files via upload
    Milestone 05

--- MyTodolistManager/app/src/main/java/com/example/egipcy/mytodolistmanager/MainActivity.java
+++ MyTodolistManager/app/src/main/java/com/example/egipcy/mytodolistmanager/MainActivity.java
@@ -5,32 +5,53 @@ import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
-public class MainActivity extends ListActivity {
+public class MainActivity extends ListActivity implements View.OnClickListener {
+
+    ListView list;
+    ArrayList<Task> array;
+    MyListAdapter mSchedule;
+    Button btn_task;
+    EditText et_task;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView list = getListView();
+        this.list = getListView();
 
-        ArrayList<Task> array = new ArrayList<Task>();
-        Task task1 = new Task();
-        task1.setName("Task #1");
-        task1.setChecked(false);
+        this.array = new ArrayList<Task>();
 
-        Task task2 = new Task();
-        task2.setName("Task #2");
-        task2.setChecked(true);
+        this.mSchedule = new MyListAdapter(this.getBaseContext(), this.array);
 
-        array.add(task1);
-        array.add(task2);
-        MyListAdapter mSchedule = new MyListAdapter(this.getBaseContext(), array);
+        this.list.setAdapter(this.mSchedule);
 
-        list.setAdapter(mSchedule);
+        this.btn_task = (Button) findViewById(R.id.btn_task);
+        this.btn_task.setOnClickListener(this);
+
+        this.et_task = (EditText) findViewById(R.id.et_task);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        switch(v.getId())
+        {
+            case R.id.btn_task:
+                Task new_task = new Task();
+                new_task.setName(this.et_task.getText().toString());
+                this.array.add(new_task);
+                this.mSchedule.notifyDataSetChanged();
+
+                this.et_task.setText("");
+                break;
+        }
     }
 }
